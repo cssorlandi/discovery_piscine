ee79c389daf76582385efe67a1a2a68d7054d624
Claudio Orlandi <claudiosorlandi@gmail.com>
2025-10-01 01:31:46 +0200

    feat: implement module 1 exercises

---
+++ module1/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Claudio"
+last_name = "Orlandi"
+
+print(first_name, last_name)
---
+++ module1/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Claudio"
+last_name = "Orlandi"
+whole_name = first_name + " " + last_name
+
+print(whole_name)
